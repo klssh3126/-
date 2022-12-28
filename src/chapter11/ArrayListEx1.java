72eed7b3d7e9f57e442eb4339f39bd94b23b3f49
klssh <klssh@DESKTOP-FBV9ABO>
2022-12-29 00:50:03 +0900

    ex1 공부

--- src/chapter11/ArrayListEx1.java
+++ src/chapter11/ArrayListEx1.java
@@ -1,6 +1,7 @@
 package chapter11;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListEx1 {
 	public static void main(String[] args) {
@@ -11,14 +12,37 @@ public class ArrayListEx1 {
 		list1.add(0);
 		list1.add(1);
 		list1.add(3);
-		
+
 		ArrayList list2 = new ArrayList(list1.subList(1, 4));
 		print(list1, list2);
-		
-		
+		Collections.sort(list1);
+		Collections.sort(list2);
+		print(list1, list2);
+
+		System.out.println("list1.containsAll(list2):" + list1.containsAll(list2));
+		list2.add("B");
+		list2.add("C");
+		list2.add(3, "A");
+		print(list1, list2);
+
+		list2.set(3, "AA");
+		print(list1, list2);
+
+		// list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
+		System.out.println("list1.retainAll(list2):" + list1.retainAll(list2));
+		print(list1, list2);
+
+		// list2에서 lis]t1에 포함된 객체들을 삭제한다.
+		for (int i =0  ; i <=list2.size() - 1; i++) {
+			if (list1.contains(list2.get(i)))
+				list2.remove(i);
+		}
+		print(list1, list2);
 	}
-	
-	static void print(ArrayList list1,ArrayList list2) {
-		
+
+	static void print(ArrayList list1, ArrayList list2) {
+		System.out.println("list1:" + list1);
+		System.out.println("list2:" + list2);
+		System.out.println();
 	}
 }
